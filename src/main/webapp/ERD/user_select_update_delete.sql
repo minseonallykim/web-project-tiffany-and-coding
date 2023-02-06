cfcf85034660f03f33ca086dff33eacd6e1bc6e2
민모 <@192.168.123.109>
2023-02-07 00:16:19 +0900

    ..

--- src/main/webapp/ERD/user_select_update_delete.sql
+++ src/main/webapp/ERD/user_select_update_delete.sql
@@ -5,10 +5,12 @@ select*from userinfo;
 --id로 회원찾기--
 select*from userinfo where userid = 'test3';
 
+/***************userinfo update**************/
 --회원정보수정(아이디변경 불가)--
 update userinfo set password = 'aaaa1', name = '이름수정', email = 'update@google.com', phone = '010-1234-5678', address = '경기도'
 where userid='test1';
 
+/***************userinfo delete**************/
 --회원탈퇴,회원정보 모두삭제--
 delete from userinfo where userid='test2';
 
