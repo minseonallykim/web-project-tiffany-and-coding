fec3c96122319cef81347d40f2dc525032ed2a1e
meltingong <hwk1210@naver.com>
2023-02-10 13:53:55 +0900

    ..

--- src/main/webapp/ERD/product_select_update_delete.sql
+++ src/main/webapp/ERD/product_select_update_delete.sql
@@ -15,3 +15,5 @@ select * from product p join category c on p.ca_no = c.ca_no and p.ca_no = 1;
 select * from product p join category c on p.ca_no = c.ca_no and c.ca_name = '반지';
 --상품명 키워드로 검색
 select * from product where p_name like '%링'; 
+--이름으로 검색된 상품 오름차순 정렬
+select * from (select rownum rn,e.* from (select * from product order by p_price asc) e) where p_name like ? 
